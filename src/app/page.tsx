c7f22425290c9c361f4a946564016b621f213b5f
chandima <chandimar.online@gmail.com>
2023-10-11 22:12:06 +0530

    feat: complete navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 
 const Page = () => (
-  <div>
+  <div className='bg-slate-900 overflow-hidden'>
     <Navbar />
     <Hero />
     <About />
