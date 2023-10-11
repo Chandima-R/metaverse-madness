9fef0a3aac53b5977eb04cb0cf1df55a28baaffc
chandima <chandimar.online@gmail.com>
2023-10-11 20:31:53 +0530

    feat: add font styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://stijndv.com" />
+        <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Next.js + TypeScript</h1>
-    </main>
-  )
-}
+import { Footer, Navbar } from '../components';
+import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
+
+const Page = () => (
+  <div>
+    <Navbar />
+    <Hero />
+    <About />
+    <Explore />
+    <GetStarted />
+    <WhatsNew />
+    <World />
+    <Insights />
+    <Feedback />
+    <Footer />
+  </div>
+);
+
+export default Page;
